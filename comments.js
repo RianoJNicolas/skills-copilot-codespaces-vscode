acf07bd6a6fe5b6057acc4633304b67d149dac45
Johan Nicolas Riano Torres <rianojnicolas@gmail.com>
2023-11-26 12:57:19 +0000

    Update Copilot third commit

--- comments.js
+++ comments.js
@@ -1,111 +1,62 @@
-// Create a web server for comments with node.js
+// Create server web
 
-// Import modules
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
 var http = require('http');
-var url = require('url');
-var fs = require('fs');
 
-// Create server
-http.createServer(function (request, response) {
-    // Parse the request url
-    var url_parts = url.parse(request.url, true);
+// connect to database
+const db = require('../database/index.js');
 
-    // Get the file name
-    var filename = url_parts.pathname.substring(1);
+// middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-    // Get the file extension
-    var ext = filename.split('.').pop();
+// serve static files
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    // Get the file type
-    var type = 'text/html';
-    switch(ext) {
-        case 'css':
-            type = 'text/css';
-            break;
-        case 'js':
-            type = 'application/javascript';
-            break;
-        case 'png':
-            type = 'image/png';
-            break;
+// routes
+app.get('/comments', (req, res) => {
+  db.getComments((err, data) => {
+    if (err) {
+      console.log('error retrieving comments');
+      res.status(404).send();
+    } else {
+      console.log('retrieved comments');
+      res.status(200).send(data);
     }
+  });
+});
 
-    // Read the file
-    fs.readFile(filename, function(err, content) {
-        // If the file doesn't exist
-        if(err) {
-            // Return a 404 error
-            response.writeHead(404, {'Content-Type': 'text/plain'});
-            response.write('File not found');
-            response.end();
-        }
-        // If the file exists
-        else {
-            // Return the file
-            response.writeHead(200, {'Content-Type': type});
-            response.write(content);
-            response.end();
-        }
-    });
-}).listen(8080);
-
-// Create the comment list
-var comments = [];
-
-// Create the server for comments
-http.createServer(function (request, response) {
-    // Parse the request url
-    var url_parts = url.parse(request.url, true);
-
-    // Get the file name
-    var filename = url_parts.pathname.substring(1);
-
-    // Get the file extension
-    var ext = filename.split('.').pop();
-
-    // Get the file type
-    var type = 'text/html';
-    switch(ext) {
-        case 'css':
-            type = 'text/css';
-            break;
-        case 'js':
-            type = 'application/javascript';
-            break;
-        case 'png':
-            type = 'image/png';
-            break;
+app.post('/comments', (req, res) => {
+  db.postComment(req.body, (err) => {
+    if (err) {
+      console.log('error posting comment');
+      res.status(404).send();
+    } else {
+      console.log('posted comment');
+      res.status(201).send();
     }
+  });
+});
 
-    // If the url is /comments
-    if(filename == 'comments') {
-        // If the request method is GET
-        if(request.method == 'GET') {
-            // Return the comments
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.write(JSON.stringify(comments));
-            response.end();
-        }
-        // If the request method is POST
-        else if(request.method == 'POST') {
-            // Get the comment text
-            var body = '';
-            request.on('data', function (data) {
-                body += data;
-            });
-            request.on('end', function () {
-                var comment = JSON.parse(body);
-                // Add the comment to the list
-                comments.push(comment);
-                // Return the comment
-                response.writeHead(200, {'Content-Type': 'application/json'});
-                response.write(JSON.stringify(comment));
-                response.end();
-            });
-        }
+app.delete('/comments', (req, res) => {
+  db.deleteComment(req.body, (err) => {
+    if (err) {
+      console.log('error deleting comment');
+      res.status(404).send();
+    } else {
+      console.log('deleted comment');
+      res.status(204).send();
     }
-}
-).listen(8081);
+  });
+});
 
-// Print to the console
-console.log('Server running at http://localhost:8080/');
+// create a webserver using http
+var server  = http.createServer(app);
+server.listen(PORT);
+
+// app listen also rises a problem
+// app.listen(PORT, () => console.log(`listening on port ${PORT}`));
