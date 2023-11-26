4774ef591893c8baf44b7c3b7e940cef038c193d
Johan Nicolas Riano Torres <rianojnicolas@gmail.com>
2023-11-26 13:16:29 +0000

    Updae Copilot third commit 2.0

--- comments.js
+++ comments.js
@@ -1,62 +1,59 @@
-// Create server web
-
+// Create web server to the start of your comments.js file
 const express = require('express');
-const app = express();
-const path = require('path');
-const PORT = 3000;
-var http = require('http');
+const router = express.Router();
+const data = require('../data');
+const commentData = data.comments;
+const postData = data.posts;
 
-// connect to database
-const db = require('../database/index.js');
-
-// middleware
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// serve static files
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// routes
-app.get('/comments', (req, res) => {
-  db.getComments((err, data) => {
-    if (err) {
-      console.log('error retrieving comments');
-      res.status(404).send();
-    } else {
-      console.log('retrieved comments');
-      res.status(200).send(data);
-    }
-  });
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await postData.getPostById(req.params.id);
+    const comments = post.comments;
+    res.json(comments);
+  } catch (e) {
+    res.status(404).json({ error: 'Post not found' });
+  }
 });
 
-app.post('/comments', (req, res) => {
-  db.postComment(req.body, (err) => {
-    if (err) {
-      console.log('error posting comment');
-      res.status(404).send();
-    } else {
-      console.log('posted comment');
-      res.status(201).send();
-    }
-  });
+router.get('/comment/:id', async (req, res) => {
+  try {
+    const comment = await commentData.getCommentById(req.params.id);
+    res.json(comment);
+  } catch (e) {
+    res.status(404).json({ error: 'Comment not found' });
+  }
 });
 
-app.delete('/comments', (req, res) => {
-  db.deleteComment(req.body, (err) => {
-    if (err) {
-      console.log('error deleting comment');
-      res.status(404).send();
-    } else {
-      console.log('deleted comment');
-      res.status(204).send();
-    }
-  });
+router.post('/:id', async (req, res) => {
+  const commentInfo = req.body;
+  try {
+    const newComment = await commentData.addComment(
+      commentInfo.name,
+      commentInfo.comment,
+      req.params.id
+    );
+    res.json(newComment);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 });
 
-// create a webserver using http
-var server  = http.createServer(app);
-server.listen(PORT);
+router.delete('/:id', async (req, res) => {
+  try {
+    await commentData.getCommentById(req.params.id);
+  } catch (e) {
+    res.status(404).json({ error: 'Comment not found' });
+    return;
+  }
+  try {
+    const post = await postData.getPostByCommentId(req.params.id);
+    const comment = await commentData.getCommentById(req.params.id);
+    await commentData.removeComment(req.params.id);
+    await postData.removeCommentFromPost(post._id, comment._id);
+    res.sendStatus(200);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
 
-// app listen also rises a problem
-// app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+module.exports = router;
